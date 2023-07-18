ce2d52229de1eb1bfbb52884582f74994b0ef078
Facundo Perez Brizuela <fperezbrizuela@technisys.com>
2023-07-18 17:21:53 -0300

    My projects button fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,41 +1,64 @@
-import React from 'react'
+import React from "react";
 
-import profile from '../../assets/profile.png'
-import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
+import profile from "../../assets/profile.png";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 const About = () => {
   return (
-    <section id='about' className='flex flex-col justify-center items-center mb-5 mt-20 '>
-      <h2 className='font-bold text-[60px] sm:text-[100px] mb-10'>About</h2>
-      <div className='flex flex-col lg:flex-row items-center gap-10 max-w-[300px] lg:max-w-[1000px] sm:max-w-[600px]'>
-        <div className='flex flex-col-reverse justify-evenly items-center gap-5'>
-          <div className='flex gap-10'>
-            <a href="https://www.linkedin.com/in/facuperezbri/" target='_blank' rel="noreferrer" className='hover:-translate-y-1 duration-300'>
+    <section
+      id="about"
+      className="flex flex-col justify-center items-center mb-5 mt-20 "
+    >
+      <h2 className="font-bold text-[60px] sm:text-[100px] mb-10">About</h2>
+      <div className="flex flex-col lg:flex-row items-center gap-10 max-w-[300px] lg:max-w-[1000px] sm:max-w-[600px]">
+        <div className="flex flex-col-reverse justify-evenly items-center gap-5">
+          <div className="flex gap-10">
+            <a
+              href="https://www.linkedin.com/in/facuperezbri/"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:-translate-y-1 duration-300"
+            >
               <FaLinkedinIn size={40} />
             </a>
-            <a href="https://github.com/facuperezbri" target='_blank' rel="noreferrer" className='hover:-translate-y-1 duration-300 '>
+            <a
+              href="https://github.com/facuperezbri"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:-translate-y-1 duration-300 "
+            >
               <FaGithub size={40} />
             </a>
           </div>
-          <img src={profile} alt='Facu Pérez Brizuela' className='w-[200px] sm:w-[400px] lg:w-[1000px] rounded-full bg-yellow-400' />
+          <img
+            src={profile}
+            alt="Facu Pérez Brizuela"
+            className="w-[200px] sm:w-[400px] lg:w-[1000px] rounded-full bg-yellow-400"
+          />
         </div>
-        <div className='flex flex-col gap-4'>
-          <h3 className='text-[35px] font-bold'>
-            I make apps and websites,<br className='hidden sm:block' /> and I do it well.
+        <div className="flex flex-col gap-4">
+          <h3 className="text-[35px] font-bold">
+            I make apps and websites.
+            <br className="hidden sm:block" />
           </h3>
-          <p className='font-light'>
-            I have always been passionate about technology. My life led me to train in different areas, specifically those related to business management, giving me an important background in finance.
+          <p className="font-light">
+            I have always been passionate about technology. My life led me to
+            train in different areas, specifically those related to business
+            management, giving me an important background in finance.
           </p>
-          <p className='font-light'>
-            Until one day I decided to go back studying what I was really passionate about and completed an intensive bootcamp of +800 hours in 4 months, graduating with solid knowledge of Javascript, React, Redux, SQL, PostgreSQL, Node.js and Express.
+          <p className="font-light">
+            Until one day I decided to go back studying what I was really
+            passionate about and completed an intensive bootcamp of +800 hours
+            in 4 months, graduating with solid knowledge of Javascript, React,
+            Redux, SQL, PostgreSQL, Node.js and Express.
           </p>
-          <p className='font-light'>
+          <p className="font-light">
             In my free time I enjoy playing drums and guitar.
           </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Button from '../ComponentsUI/Button'
-import memoji from '../../assets/memoji.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../ComponentsUI/Button";
+import memoji from "../../assets/memoji.png";
 
 const Hero = () => (
-  <section id='hero' className='h-[100%] w-[100vw] max-w-full flex flex-col justify-around ss:justify-between text-center gap-10 xxs:gap-16 xs:gap-12 md:gap-10'>
+  <section
+    id="hero"
+    className="h-[100%] w-[100vw] max-w-full flex flex-col justify-around ss:justify-between text-center gap-10 xxs:gap-16 xs:gap-12 md:gap-10"
+  >
     <div>
       <p className="text-[40px]">👋🏼</p>
-      <p className='font-thin sm:text-[20px]'>my name is Facu and I'm a</p>
-      <h1 className='inline font-bold text-[38px] ss:text-[50px] md:text-[70px] xl:text-[100px] m-1 tracking-tight duration-500 fullstack animate__animated animate__pulse'>Fullstack Developer</h1>
-      <h1 className='inline font-bold text-[38px] ss:text-[50px] md:text-[70px] xl:text-[100px] m-1 tracking-tight strokeme duration-500 hover:text-black animate__animated animate__pulse animate__delay-1s'><br />& UX/UI Designer</h1>
-      <p className='font-thin mt-3 sm:text-[20px]'>currently based in Mendoza, Argentina.</p>
-      <div className='flex gap-3 justify-center mt-10 md:mt-3'>
-        <Button className={'bg-black text-white'}><Link to={'projects'}>My projects</Link></Button>
-        <Button><a href='mailto:facundo.perez.brizuela@gmail.com'>Contact Me</a></Button>
+      <p className="font-thin sm:text-[20px]">my name is Facu and I'm a</p>
+      <h1 className="inline font-bold text-[38px] ss:text-[50px] md:text-[70px] xl:text-[100px] m-1 tracking-tight duration-500 fullstack animate__animated animate__pulse">
+        Fullstack Developer
+      </h1>
+      <h1 className="inline font-bold text-[38px] ss:text-[50px] md:text-[70px] xl:text-[100px] m-1 tracking-tight strokeme duration-500 hover:text-black animate__animated animate__pulse animate__delay-1s">
+        <br />& UX/UI Designer
+      </h1>
+      <p className="font-thin mt-3 sm:text-[20px]">
+        currently based in Mendoza, Argentina.
+      </p>
+      <div className="flex gap-3 justify-center mt-10 md:mt-3">
+        <Link to={"projects"}>
+          <Button className={"bg-black text-white"}>My projects</Button>
+        </Link>
+        <Button>
+          <a href="mailto:facundo.perez.brizuela@gmail.com">Contact Me</a>
+        </Button>
       </div>
-
     </div>
-    <img src={memoji} alt='Facu Perez Brizuela' className='h-[100%] max-h-[250px] xl:max-h-[300px] object-contain grad' />
-
+    <img
+      src={memoji}
+      alt="Facu Perez Brizuela"
+      className="h-[100%] max-h-[250px] xl:max-h-[300px] object-contain grad"
+    />
   </section>
-)
+);
 
-
-export default Hero
+export default Hero;
