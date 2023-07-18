ecc83882734fbd91c946d619f30f1e3a2fa79941
Facundo Perez Brizuela <fperezbrizuela@technisys.com>
2023-07-18 17:25:29 -0300

    Skills margin bottom added

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { skills } from '../../constants'
+import React from "react";
+import { skills } from "../../constants";
 
 const Skills = () => {
   return (
-    <section id='skills' className='flex flex-col justify-center items-center m-auto'>
-      <h2 className='font-bold text-[60px] sm:text-[100px] mb-10'>Skills</h2>
-      <div className='flex items-center justify-center  flex-wrap max-w-[100%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[45%] gap-10 '>
-        {
-          skills.map((s, i) => (
-            <div className='flex flex-col items-center justify-center gap-10'>
-              <img className='w-[100px] h-[100px] object-contain hover:-translate-y-2 duration-200' src={s.icon} alt={s.title} key={i} />
-              < p > {s.title}</ p>
-            </ div>
-          ))
-        }
+    <section
+      id="skills"
+      className="flex flex-col justify-center items-center m-auto mb-4"
+    >
+      <h2 className="font-bold text-[60px] sm:text-[100px] mb-10">Skills</h2>
+      <div className="flex items-center justify-center  flex-wrap max-w-[100%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[45%] gap-10 ">
+        {skills.map((s, i) => (
+          <div className="flex flex-col items-center justify-center gap-10">
+            <img
+              className="w-[100px] h-[100px] object-contain hover:-translate-y-2 duration-200"
+              src={s.icon}
+              alt={s.title}
+              key={i}
+            />
+            <p> {s.title}</p>
+          </div>
+        ))}
       </div>
+    </section>
+  );
+};
 
-    </section >
-  )
-}
-
-export default Skills
+export default Skills;
